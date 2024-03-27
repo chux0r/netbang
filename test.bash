7a8daf9e184a24bb6cb7e10658b1399928083a8a
chux0r <gravitr0n@protonmail.com>
2024-03-27 17:31:00 -0500

    minor update: bash test script

--- test.bash
+++ test.bash
@@ -15,24 +15,47 @@
 
 echo "Build test by: ${USER}@${HOSTNAME}:${PWD}" 
 echo -n "Date:" && date
-echo -n "GO version" && go version
-echo -n "Platform OS/arch details:" && uname -a
-echo "go build ./netbang.go ./ifstat.go ./portfu.go ./recon.go ./resolver.go"
-go build ./netbang.go ./ifstat.go ./portfu.go ./recon.go ./resolver.go
-echo "NETBANG TEST CASES:"
+echo -n " -- GO version: " && go version
+echo -n " -- Platform OS/arch details: " && uname -a
+
+
+printf "\n\n=============BUILD: go build ./netbang.go ./recon.go ./resolver.go"
+
+go build ./netbang.go ./recon.go ./resolver.go
+
+
+printf "\nNETBANG TEST CASES:\n"
 echo "./netbang"
+
 ./netbang 
-echo "=============EXECUTE: ./netbang 127.0.0.1"
-./netbang 127.0.0.1 #default tcp scan, using portlist "tcp_short"
-echo "=============EXECUTE: \"53,161,10000\" -> ../netbang_ports.tmp"
-echo "./netbang --proto udp --portsfile ../netbang_ports.tmp -t 500 127.0.0.1 && rm ../netbang_ports.tmp"
-echo -n "53,161,10000" > ../netbang_ports.tmp && echo -n "../netbang_ports.tmp:" && cat ../netbang_ports.tmp
+
+
+printf "\n=============BASIC SCAN-BY-HOSTNAME: ./netbang scanme.org\n"
+
+./netbang scanme.org #default tcp scan, using portlist "tcp_short"
+
+
+printf "\n=============UDP SCAN, PORTS DEFINED IN FILE, CUSTOM TIMEOUT ./netbang --proto udp --portsfile ../netbang_ports.tmp -t 500 127.0.0.1\n"
+echo -n "53,161,10000" > ../netbang_ports.tmp && printf "\n\tFILE INJECT ../netbang_ports.tmp:" && cat ../netbang_ports.tmp && printf "\n"
+
 ./netbang --proto udp --portsfile ../netbang_ports.tmp -t 500 127.0.0.1 && rm ../netbang_ports.tmp ## tcp scan, ports defined in file
-echo "=============EXECUTE: ./netbang --recon list"
+
+
+printf "\n=============EXECUTE: ./netbang --recon list\n"
+
 ./netbang --recon list # list recon modes
-echo "=============EXECUTE: ./netbang --recon dns amazon.com"
-./netbang --recon dns scanme.org # get dns info
-echo "=============EXECUTE: ./netbang --recon dns --ns 8.8.8.8 github.com"
+
+
+printf "\n=============EXECUTE: ./netbang --recon dns amazon.com\n"
+
+./netbang --recon dns amazon.com # get dns info
+
+
+printf "\n=============EXECUTE: ./netbang --recon dns --ns 8.8.8.8 github.com\n"
+
 ./netbang --recon dns --ns 8.8.8.8 github.com #get dns info, use custom resolver
-echo "=============EXECUTE: ./netbang --recon shodan hostip 1.1.1.1"
+
+
+printf "\n=============EXECUTE: ./netbang --recon shodan hostip 1.1.1.1\n"
+
 ./netbang --recon shodan hostip 1.1.1.1 # query shodan data using host ip NOTE: this test only works when $SHODAN_KEY is defined/valid  
